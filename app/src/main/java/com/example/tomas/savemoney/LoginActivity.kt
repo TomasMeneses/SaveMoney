6916295861922ee40ab9aee385952ef9b7afa294
tomas <tomasmeneses22@gmail.com>
2018-11-07 20:40:58 -0300

    iniciando tela Home

--- app/src/main/java/com/example/tomas/savemoney/LoginActivity.kt
+++ app/src/main/java/com/example/tomas/savemoney/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.example.tomas.savemoney
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+
+            val i = Intent(this, HomeActivity::class.java)
+            startActivity(i)
+        }
     }
 }
