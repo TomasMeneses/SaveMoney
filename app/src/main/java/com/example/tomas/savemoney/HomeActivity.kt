710860e19398a92a0c8c9ad3b1fea23ec2bfe78f
tomas <tomasmeneses22@gmail.com>
2018-11-09 11:21:32 -0300

    Menu inferior criado

--- app/src/main/java/com/example/tomas/savemoney/HomeActivity.kt
+++ app/src/main/java/com/example/tomas/savemoney/HomeActivity.kt
@@ -2,11 +2,15 @@ package com.example.tomas.savemoney
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+
     }
 }
