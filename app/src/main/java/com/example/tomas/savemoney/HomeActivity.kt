4ed16d4d88f4f3aa401b8ad547c5d4822f7d2901
tomas <tomasmeneses22@gmail.com>
2018-11-10 16:52:44 -0300

    gráfico implementado

--- app/src/main/java/com/example/tomas/savemoney/HomeActivity.kt
+++ app/src/main/java/com/example/tomas/savemoney/HomeActivity.kt
@@ -1,8 +1,12 @@
 package com.example.tomas.savemoney
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_home.*
+import com.razerdp.widget.animatedpieview.AnimatedPieView
+import com.razerdp.widget.animatedpieview.AnimatedPieViewConfig
+import com.razerdp.widget.animatedpieview.data.SimplePieInfo
 
 class HomeActivity : AppCompatActivity() {
 
@@ -10,7 +14,22 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        desenharGrafico()
 
+    }
 
+    fun desenharGrafico(){
+        var graficoPizza: AnimatedPieView = findViewById(R.id.graficoPizza)
+        var configuracao: AnimatedPieViewConfig
+        configuracao = AnimatedPieViewConfig()
+        configuracao.startAngle((-90.0).toFloat())
+
+            configuracao.addData(SimplePieInfo(30.0, Color.parseColor("#ff0000"), "teste 1"))
+            configuracao.addData(SimplePieInfo(18.0, Color.parseColor("#00ff00"), "teste 2")).drawText(true)
+            configuracao.duration(2000).textSize(40.toFloat())
+
+// The following two sentences can be replace directly 'mAnimatedPieView.start (config); '
+        graficoPizza.applyConfig(configuracao);
+        graficoPizza.start();
     }
 }
