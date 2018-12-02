54a1d92ea8be91975d31d46256e74134321fd188
Carlos <azevedo1236@gmail.com>
2018-12-02 19:13:41 -0300

    Implementacão do recycle view na tela Home.

---
+++ app/src/main/java/com/example/tomas/savemoney/DespesasAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.tomas.savemoney
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class DespesasAdapter: RecyclerView.Adapter<CustomViewHolder>(){
+
+    //numero de itens a serem exibidos por vez
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        //Como criamos a view
+        val layoutInflater = LayoutInflater.from(parent?.context)
+        val cellForRow = layoutInflater.inflate(R.layout.despesas_row, parent, false)
+        return CustomViewHolder(cellForRow)
+    }
+
+    override fun onBindViewHolder(p0: CustomViewHolder, p1: Int) {
+
+    }
+}
+
+class CustomViewHolder(v: View): RecyclerView.ViewHolder(v){
+
+}
--- app/src/main/java/com/example/tomas/savemoney/HomeActivity.kt
+++ app/src/main/java/com/example/tomas/savemoney/HomeActivity.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.LinearLayout
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -17,55 +19,8 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val listView = findViewById<ListView>(R.id.ListaPrincipal)
 
-        listView.adapter = MyCustomAdapter(this)
-
-        imageViewAdd.setOnClickListener {
-            val i = Intent(this, KeyboardActivity::class.java)
-            startActivity(i)
-        }
-
-        imageViewPerfil.setOnClickListener {
-            val j = Intent(this, PerfilActivity::class.java)
-            startActivity(j)
-        }
-        imageViewgraf.setOnClickListener {
-            val j = Intent(this, GraficosActivity::class.java)
-            startActivity(j)
-        }
-
-
-    }
-
-    private class MyCustomAdapter(context: Context): BaseAdapter(){
-
-        private val mContext: Context
-
-        init {
-            mContext = context
-        }
-
-        override fun getCount(): Int {
-            return 5
-        }
-
-        override fun getItem(position: Int): Any {
-            return "Teste"
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
-            val layoutInflater = LayoutInflater.from(mContext)
-            val rowMain = layoutInflater.inflate(R.layout.receitas_despesas, viewGroup, false)
-            return rowMain
-
-//            val textView = TextView(mContext)
-//            textView.text = "Essa é minha linha"
-//            return textView
-        }
+        recycleView_main.layoutManager = LinearLayoutManager(this)
+        recycleView_main.adapter = DespesasAdapter()
     }
 }
